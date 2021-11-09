1285ce0fd3655e13efd5cc3d2f8095fe0a6b3621
andydangit <andy.dang.it@gmail.com>
2021-11-09 10:45:19 -0600

    showing the byte anf float min and max number

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,7 +8,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World");
-            Console.WriteLine();
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
         }
     }
 }
