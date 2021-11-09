8b8c5825b7d8ba4fa00abb58f329b2ed8176c434
andydangit <andy.dang.it@gmail.com>
2021-11-09 10:42:00 -0600

    Remove all the Var

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,13 +8,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World");
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Andy";
-            bool isWorking = true;
-            Console.WriteLine(number);
+            Console.WriteLine();
         }
     }
 }
