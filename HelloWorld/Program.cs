8f13329557694fa03b43d5b17b5103d0b69bb62b
andydangit <andy.dang.it@gmail.com>
2021-11-09 11:41:07 -0600

    Convert to string

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,9 +8,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World");
-            int i = 1000;
-            byte b = (byte) i;
-            Console.WriteLine(b);
+            var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
         }
     }
 }
