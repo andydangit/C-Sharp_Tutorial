b6a643a254bf2ab9cca4067e8cc9115c3bda0d98
andydangit <andy.dang.it@gmail.com>
2021-11-09 10:22:34 -0600

    types of Variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,7 +8,12 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World");
-            byte number;
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Andy";
+            bool isWorking = true;
             Console.WriteLine(number);
         }
     }
