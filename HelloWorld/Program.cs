466417bbffe51379863dad750e2c0f246a13ddf7
andydangit <andy.dang.it@gmail.com>
2021-11-09 11:56:11 -0600

    using Try Catch Exception so it does not crash

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,10 +7,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World");
-            var number = "1234";
-            int i = Convert.ToInt32(number);
-            Console.WriteLine(i);
+            try
+            {
+                var number = "1234";
+                byte i = Convert.ToByte(number);
+                Console.WriteLine(i);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte."); 
+               
+            }
+
+          
         }
     }
 }
