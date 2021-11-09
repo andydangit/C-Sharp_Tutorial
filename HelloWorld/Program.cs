8b092ab00a786c3c46a4160bd3a6a5f7a08b3ded
andydangit <andy.dang.it@gmail.com>
2021-11-09 11:26:18 -0600

    How to write variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,8 +8,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World");
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+
+            
         }
     }
 }
